09c7088a09de5b74a59d4db6df4a9b4bcc5f9dc5
natanlimap <natanlimap@gmail.com>
2020-03-02 18:16:58 -0300

    UI chat finished

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# uichat
+# About the project
 
-A new Flutter application.
+## User interface Project #3: Implementing a UI Card button
+In this project was done a User interface chat with Flutter as framework.
+The "User interface project" is a collection of simples UI widgets design based on a User Experience knowledge. You can check the others on my github profile, they all repositories start with "UI-".
 
-## Getting Started
+## Screenshot
+<img src="https://github.com/Natanlimap/UI-Chat/blob/master/Screenshot/uichat.png" width="30%"  height="30%" />
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Author
+Natan Pereira - (https://github.com/Natanlimap)
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UI CHAT',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -60,131 +60,158 @@ class _ChatPageState extends State<ChatPage> {
 
     _listener(){ //function that will add the data on the screen and if necessary scroll the page
       final stream = messagesList;
-      stream.forEach((dados){
+      stream.forEach((dados){ //parssing data for stremcontroller
         _streamController.add( dados );
       });
-      Timer(Duration(seconds: 1), (){
+      Timer(Duration(seconds: 1), (){ //wait 1 second and scroll to the end of the list view
         scrollController.jumpTo(scrollController.position.maxScrollExtent);
       });
     }
-    _listener();
-    var messagesWidget = //a var that contains the messages widgets
-        Expanded(child:
-          StreamBuilder<MessageClass>( //A stream builder for simultaneous addition of data as widget
-              stream: _streamController.stream,
-              builder: (context, snapshot) {
-                if(snapshot.hasData){ //check if stream ref exists
-                  print(snapshot.hasData); //checking if this ref has data
-                  switch (snapshot.connectionState) { //checking the state of snapshot by stream addition
-                    case ConnectionState.none:
-                    case ConnectionState.waiting:
-                      return Center(child: CircularProgressIndicator(),); //Loading widget
-                    case ConnectionState.active:
-                    case ConnectionState.done:
-                    return ListView.builder( //A listView with the messages
-                          controller: scrollController, //ScrollController for the current position of list view
-                          itemCount: messagesList.length, //Getting the list lenght
-                          itemBuilder: (context, index) {
-                            Alignment _textAlign; //text alignment(me / you)
-                            Color _color; //text color
-                            if (messagesList[index].whosent == "me") { //checking who sent the message
-                              _textAlign = Alignment.centerRight; //aligning message if i sent
-                              _color = Colors.red[300]; //changing the color
-                            } else {
-                              _color = Colors.black26; //aligning message if other sent
-                              _textAlign = Alignment.centerLeft; //changing the color
-                            }
-                            return Align(
-                              alignment: _textAlign,
-                              child: Padding(
-                                padding: EdgeInsets.all(6),
-                                child: Container( //message container
-                                  width: width * 0.8,
-                                  padding: EdgeInsets.all(16),
-                                  decoration: BoxDecoration( //putting the decoration
-                                      color: _color,
-                                      borderRadius: BorderRadius.all( //rounding the container
-                                          Radius.circular(8))),
-                                  child: Text( //message content
-                                    messagesList[index].message,
-                                    style: TextStyle(fontSize: 18),
-                                  ),
+    _listener(); //calling listener
+
+    Widget messagesWidget () { //contains the messages widgets
+      return new Expanded(child:
+        new StreamBuilder<
+          MessageClass>( //A stream builder for simultaneous addition of data as widget
+          stream: _streamController.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) { //check if stream ref exists
+              print(snapshot.hasData); //checking if this ref has data
+              switch (snapshot
+                  .connectionState) { //checking the state of snapshot by stream addition
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return new Center(
+                    child: new CircularProgressIndicator(),); //Loading widget
+                case ConnectionState.active:
+                case ConnectionState.done:
+                  return new ListView.builder( //A listView with the messages
+                      controller: scrollController, //ScrollController for the current position of list view
+                      itemCount: messagesList.length,  //Getting the list lenght
+                      itemBuilder: (context, index) {
+                        Alignment _textAlign; //text alignment(me / you)
+                        Color _color; //text color
+                        if (messagesList[index].whosent == "me") { //checking who sent the message
+                          _textAlign = Alignment.centerRight; //aligning message if i sent
+                          _color = Colors.black87; //changing the color
+                        } else {
+                          _color =
+                              Colors.grey[800]; //aligning message if other sent
+                          _textAlign =
+                              Alignment.centerLeft; //changing the color
+                        }
+                        return new Align(
+                          alignment: _textAlign,
+                          child: new Padding(
+                            padding: new EdgeInsets.all(6),
+                            child: new Card(
+                              elevation: 10,
+                                child: new Container( //message container
+                                width: width * 0.8,
+                                padding: new EdgeInsets.all(16),
+                                decoration: new BoxDecoration( //putting the decoration
+                                    color: _color,
+                                    borderRadius: new BorderRadius
+                                        .all( //rounding the container
+                                        new Radius.circular(8))),
+                                child: new Text( //message content
+                                  messagesList[index].message,
+                                  style: new TextStyle(fontSize: 18, color: Colors.white),
                                 ),
                               ),
-                            );
-                          }
-                      );
-                  }
-                      }else{
-                        return Container(); //if doesnt has data
+                            )
+                          ),
+                        );
                       }
-                }
-              )
-        );
+                  );
+              }
+            } else {
+              return new Container(); //if doesnt has data return a white container
+            }
+          }
+      )
+      );
+    }
+    addMessage(){ //function that send message to list
+      messagesList.add(MessageClass(_controllerMessage.text, "me")); //implement here a method to send to your database instead the list
+      _controllerMessage.clear();
+      _listener();
+    }
 
 
-    var caixaDeMensage = Container(
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(left: width * 0.02),
-          ),
-          Expanded(
-            child: TextField(
-              autofocus: false,
-              keyboardType: TextInputType.text,
-              controller: _controllerMessage,
-              style: TextStyle(fontSize: 20),
-              decoration: InputDecoration(
-                  hintText: "Digite sua mensagem",
-                  prefixIcon: Icon(
-                    Icons.monetization_on,
-                    color: Colors.black45,
-                  ),
-                  filled: true,
-                  hoverColor: Colors.black45,
-                  fillColor: Colors.white,
-                  focusColor: Colors.black45,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black45),
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(100),
-                  )),
+    Widget bottomWidget() { //widget with message formfield and sent buttom
+      return new Container(
+        child: new Row(
+          children: <Widget>[
+            new Padding(
+              padding: new EdgeInsets.only(left: width * 0.02),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: width * 0.02),
-          ),
-          FloatingActionButton(
-            backgroundColor: Colors.black45,
-            child: Icon(Icons.send),
-            onPressed: (){},
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: width * 0.02),
-          ),
-        ],
-      ),
-    );
-    return Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-          title: Row(
-            children: <Widget>[
-              CircleAvatar(backgroundColor: Colors.white,child: Icon(Icons.face, size: 40, color: Colors.red,), radius: 25,),
-              Padding(padding: EdgeInsets.only(left: width*0.02)),Text("User name", style: TextStyle(fontSize: 20),)],),
-          backgroundColor: Colors.red,
+            new Expanded(
+              child: new TextField( //Text form field where user will put the message
+                autofocus: false, //dont initiliaze the page with this form pressed
+                keyboardType: TextInputType.text,
+                controller: _controllerMessage, //controller
+                style: new TextStyle(fontSize: 20),
+                decoration: new InputDecoration( //form decoration
+                    hintText: "Type your message", hintStyle: new TextStyle(color: Colors.black54),
+                    prefixIcon: new Icon( //icon before the hint message
+                      Icons.message,
+                      color: Colors.black54,
+                    ),
+                    hoverColor: Colors.black87,
+                    focusColor: Colors.black87,
+                    focusedBorder: new OutlineInputBorder(
+                      borderSide: new BorderSide(color: Colors.black87),
+                      borderRadius: new BorderRadius.circular(100), //rounding corners
+                    ),
+                    border: new OutlineInputBorder(
+                      borderRadius:  new BorderRadius.circular(100), //rounding corners
+                    )),
+              ),
+            ),
+            new Padding(
+              padding: new EdgeInsets.only(left: width * 0.02),
+            ),
+            new FloatingActionButton( //button to sent the message
+              elevation: 10,
+              backgroundColor: Colors.black87,
+              child: new Icon(Icons.send),
+              onPressed: () {
+                addMessage();
+              },
+            ),
+            new Padding(
+              padding: new EdgeInsets.only(left: width * 0.02),
+            ),
+          ],
         ),
-        body: Container(
+      );
+    }
+    return new Scaffold( //page's body
+        appBar: new AppBar( //appbar app
+          elevation: 10,
+          leading: GestureDetector(
+            child: Icon(Icons.arrow_back_ios),
+            onTap: (){
+              //navigator pop
+            },
+          ),
+          title: new Row( //user name and photo
+            children: <Widget>[
+              new CircleAvatar(backgroundColor: Colors.white,child: new Icon(Icons.face, size: 40, color: Colors.black87,), radius: 25,),
+              new Padding(padding: EdgeInsets.only(left: width*0.02)), new Text("User name", style: new TextStyle(fontSize: 20),)
+            ],
+          ),
+          backgroundColor: Colors.black87,
+        ),
+        body: new Container( //body content
             color: Colors.white,
-          child: SafeArea(
-            child: Column(
+          child: new SafeArea( //to protect ios phones with uncompleted widgets
+            child: new Column(
               children: <Widget>[
-                messagesWidget,
-                caixaDeMensage,
-                Padding(padding: EdgeInsets.only(bottom: height * 0.02))
+                messagesWidget(),
+                bottomWidget(),
+                new Padding(padding: new EdgeInsets.only(bottom: height * 0.02))
               ],
             ),
           ),
